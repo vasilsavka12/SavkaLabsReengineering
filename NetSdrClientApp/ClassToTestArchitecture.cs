7d6931d0a3de7c607fb8959e4bd11bed9614ce92
vasilsavka12 <vasilsavka12@gmail.com>
2025-11-28 01:25:58 +0200

    Додавання архітектурного тестування та серверу
    У файл `ClassToTestArchitecture.cs` додано клас `ClassToTestArchitecture` з методом `CreateServer`, що створює екземпляр `EchoServer` на порту 5000.
    
    У файл проекту `NetSdrClientApp.csproj` додано посилання на проект `EchoServer`.
    
    У файл `Architecture.cs` додано клас `Architecture` з тестом `UI_InfrastructureTestOnDependency`, що перевіряє відсутність залежності від `EchoTspServer` з використанням `NetArchTest.Rules`.
    
    У файл проекту `NetSdrClientAppTests.csproj` видалено посилання на `NUnit` та додано посилання на `NetArchTest.Rules` для використання архітектурних правил у тестах.

---
+++ NetSdrClientApp/ClassToTestArchitecture.cs
@@ -0,0 +1,12 @@
+﻿using EchoTspServer;
+
+namespace NetSdrClientApp
+{
+    public class ClassToTestArchitecture
+    {
+        public void CreateServer()
+        {
+            var server = new EchoServer(5000);
+        }
+    }
+}
---
+++ NetSdrClientAppTests/Architecture.cs
@@ -0,0 +1,23 @@
+﻿using NetArchTest.Rules;
+using NUnit.Framework;
+
+namespace NetSdrClientAppTests
+{
+    public class Architecture
+    {
+        [Test]
+        public void UI_InfrastructureTestOnDependency()
+        {
+            var assembly = typeof(NetSdrClientApp.NetSdrClient).Assembly;
+
+            var result = Types.InAssembly(assembly)
+                .That()
+                .ResideInNamespace("NetSdrClientApp")
+                .ShouldNot()
+                .HaveDependencyOn("EchoTspServer")
+                .GetResult();
+
+            Assert.IsTrue(result.IsSuccessful);
+        }
+    }
+}
